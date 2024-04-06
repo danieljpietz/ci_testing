6dc929338be8a1b3414e1c88f1e775f4dd8517d9
danieljpietz <danieljpietz@gmail.com>
2024-04-06 11:42:10 -0700

    run qemu

--- pipeline/entrypoint.sh
+++ pipeline/entrypoint.sh
@@ -2,3 +2,12 @@
 
 echo "Hello World"
 ls
+cd repo
+cargo build
+
+qemu-system-arm \
+  -cpu cortex-m3 \
+  -machine lm3s6965evb \
+  -nographic \
+  -semihosting-config enable=on,target=native \
+  -kernel target/thumbv7m-none-eabi/debug/examples/hello
